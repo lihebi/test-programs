c79fbef85e87b9e0e94856d2b2f53b6fde2fe369
Hebi Li <lihebi.com@gmail.com>
2016-09-06 01:27:24 -0500

    whileif add more

--- cfgtest/whileif.c
+++ cfgtest/whileif.c
@@ -3,6 +3,15 @@ int main() {
   while (a<0) {
     if (a>8) {
       int true_branch;
+      continue;
     }
+    if (b=2) {
+      break;
+    }
+    if (c=1) {
+      return 2;
+    }
+    a=8;
   }
+  a=9;
 }
